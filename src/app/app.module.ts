0da6fb5c8c61a74e4ebb50ec029c491ba3c246c7
maxi <maxiranqui@gmail.com>
2020-04-19 17:53:13 -0300

    Se sube ejemplos de formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule , ReactiveFormsModule} from '@angular/forms' ;
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AltaAlumnosComponent } from './alta-alumnos/alta-alumnos.component';
+import { PersonaComponent } from './formularios/reactivos/persona.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AltaAlumnosComponent
+    AltaAlumnosComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

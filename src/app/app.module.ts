2d4406776705934a146b3a3ef63a2aa149baa4f6
maxi <maxiranqui@gmail.com>
2020-04-26 21:38:33 -0300

    Agrego formulario Profesor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AltaAlumnosComponent } from './alta-alumnos/alta-alumnos.component';
 import { PersonaComponent } from './formularios/reactivos/persona.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ProfesorComponent} from './formularios/reactivos/profesor/profesor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AltaAlumnosComponent,
-    PersonaComponent
+    PersonaComponent,
+    ProfesorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formularios/reactivos/profesor/profesor.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray} from '@angular/forms';
+
+@Component({
+	selector: 'app-profesor',
+	templateUrl: './profesor.component.html'
+})
+export class ProfesorComponent {
+	
+    constructor(private fb:FormBuilder){}
+
+	profesorForm = this.fb.group({
+		nombre: ['', Validators.required],
+		apellido: [''],
+        edad:[''],
+        materia:[''],
+		direccion: this.fb.group({
+			calle: [''],
+			numero: ['']
+        }),
+        telefonos : this.fb.array([this.fb.control('')])
+    });
+    get telefonos(){
+        return this.profesorForm.get('telefonos') as FormArray;
+    }
+    agregarTelefono(){
+        this.telefonos.push(this.fb.control(''));
+    }
+
+	submit() {
+		debugger;
+		this.profesorForm.value;
+
+		this.profesorForm.setValue({
+			nombre: '',
+			apellido: '',
+			edad: '',
+			direccion: {
+				calle: '',
+				numero: ''
+			}
+        });
+
+        this.profesorForm.patchValue({edad: 60});
+	}
+}
